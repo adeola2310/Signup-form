4972c90afbd85d99dd857181773c0d7cb86abe18
Adeola Adekoyejo <adekoyejoadeola@gmail.com>
2020-05-20 09:19:37 +0100

    design complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Link} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <div className="container">
+              <div className="content">
+                  <div className="left">
+                      <h2 className="header">Learn to code by
+                          <br/>
+                          teaching others
+                      </h2>
+                      <p className="paragraph">
+                          See how experienced developers solve problems in real-time.
+                          <br/>Watching scripted tutorials is great, but understanding how
+                          <br/>developers think is invaluable.
+                      </p>
+                  </div>
+                  <div className="right">
+                      <button className="try">
+                          <b>Try it free 7 days</b> then $20/mo.
+                      </button>
+                      <form className="form-box">
+                          <div className="inputBox">
+                              <input type="text" placeholder="First Name"/>
+                              <input type="text" placeholder="Last Name"/>
+                              <input type="email" placeholder="Email Address"/>
+                              <input type="password" placeholder="Password"/>
+                              <button className="submit">claim your free trial</button>
+                              <p className="text">By clicking the button, you are agreeing to our <Link to="">Terms and Services</Link>
+                              </p>
+                          </div>
+
+                      </form>
+                  </div>
+              </div>
+
+          </div>
+      </Router>
+
   );
 }
 
